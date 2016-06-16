029859733208daa5b82b9fc8b7a110b1b9a19ff4
tonynd <tony.dinh01@gmail.com>
2016-06-16 10:53:29 -0700

    Updated syntax

--- src/components/app.react.js
+++ src/components/app.react.js
@@ -3,16 +3,39 @@ import Child from './child.react';
 
 export default class App extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			userName:"",
+			userAddress:"",
+			userQuote:""
+		};
+	}
 
-  render() {
-    return (
-      <div className="container">
-        this is the parent component
-        <Child />
-      </div>
-    );  
-  }
+	_handleChange = (event) => {
+		let obj = {};
+		obj[event.target.name] = event.target.value;
+		this.setState(obj)
+	};
+
+	_handleClick = () => {
+		let obj = this.state;
+		console.log(obj);
+	};
+
+	render() {
+		return (
+			<div className="app">
+				<h1>React Training</h1>
+				<input type="text" name="userName" placeholder="Name" onChange = 
+					{this._handleChange}/> <br/><br/>
+				<input type="text" name="userAddress" placeholder="Address" onChange = 
+					{this._handleChange}/> <br/><br/>
+				<input type="text" name="userQuote" placeholder="Favorite Quote" 
+					onChange = {this._handleChange}/> <br/><br/><br/>
+
+				<button onClick={this._handleClick}>Submit</button>
+			</div>
+		);  
+	}
 }
